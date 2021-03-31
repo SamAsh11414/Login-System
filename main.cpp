78bcc27fb7b444d14148d2cdcf020fbb248ff6ab
Samuel Ashkenas <Sammy.ashkenas1@gmail.com>
2021-03-31 20:32:50 +0000

    Account Creation Made

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
+
+void signup() {
+  string username;
+  string password;
+
+  cout << "Please type your username";
+  cin >> username;
+
+  cout << "Please type your password";
+  cin >> password;
+
+  ofstream Logins("logins.txt");
+
+  Logins << username + "\n";
+  Logins << password;
+
+  Logins.close();
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+    signup();
 }
