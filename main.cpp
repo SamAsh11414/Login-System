754771d3fa4e44ecb0535e5f18100ef3d3a3a7e9
Samuel Ashkenas <Sammy.ashkenas1@gmail.com>
2021-03-31 21:29:32 +0000

    Added Login

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ void signup() {
   string username;
   string password;
 
-  cout << "Please type your username";
+  cout << "Please type your username\n";
   cin >> username;
 
-  cout << "Please type your password";
+  cout << "Please type your password\n";
   cin >> password;
 
   ofstream Logins("logins.txt");
@@ -20,6 +20,50 @@ void signup() {
   Logins.close();
 }
 
+void login() {
+  string username;
+  string password;
+
+  cout << "Please type your username\n";
+  cin >> username;
+
+  cout << "Please type your password\n";
+  cin >> password;
+  
+  bool founduser = false;
+  bool foundpass = false;
+  string line;
+
+  ifstream Logins("logins.txt");
+
+  while(std::getline(Logins, line)) {
+      if(line.find(username) && line.find(password) != string::npos){
+          cout << "Username Found\n";
+          cout << "Password Found\n";
+          founduser = true;
+          foundpass = true;
+  }
+}
+
+  if (!founduser){
+      cout << "Did not find username";
+  }
+    
+  if (!foundpass){
+      cout << "Did not find password";
+  }
+}
+
 int main() {
-    signup();
+    string Choice;
+    cout << "do you want to login or signup?\n";
+    cin >> Choice;
+
+    if (Choice == "login") {
+        login();
+    }
+
+    if (Choice == "signup") {
+        signup();
+    }
 }
